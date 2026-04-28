00877f3b34a192d1d1cb7b0102e1b8b5ca8040a6
madinak07 <252433@astanait.edu.kz>
2026-04-28 23:11:47 +0500

    Quick Sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -17,4 +17,38 @@ public class Sorter {
             arr[indexOfMin] = temp;
         }
     }
+
+    public void advancedSort(int[] arr) {
+        if(arr == null || arr.length == 0) return;
+        quickSort(arr, 0, arr.length - 1)
+    }
+
+    private void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pi = partition(arr, start, end);
+            quickSort(arr, start, pi - 1);
+            quickSort(arr, pi + 1, end);
+        }
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = (start - 1);
+
+        for (int j = start; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = temp;
+
+        return i + 1;
+    }
 }
